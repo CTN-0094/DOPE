5302f632230c3171af82649fec9e24d2ba27b0ca
nathaniel castor <nathancastor@gmail.com>
2021-04-12 15:43:44 -0700

    removed print statement from test-lookup and completed basic tests for lookup_syn

---
+++ tests/testthat/test-lookup_syn.R
@@ -0,0 +1,22 @@
+###  Test with multiple category matches  ###
+testOut_df <- data.frame(
+  original_word = c("zip", "zip", "zip"),
+  class = c("stimulant", "cannabis", "stimulant"),
+  category = c("cocaine", "marijuana", "methamphetamine"),
+  match = c("zip", "zip", "zip")
+)
+
+test_that("multiple category match works", {
+  expect_equal(testOut_df, lookup_syn("zip"))
+})
+
+
+testOut_df2 <- data.frame(
+  class = rep(c("cannabis"),each=5),
+  category_match = rep(c("marijuana"), each= 5),
+  synonym = c("420", "a-bomb", "acapulco gold", "acapulco red", "ace")
+)
+
+test_that("single category match works", {
+  expect_equal(testOut_df2, head(lookup_syn("draf weed"),5))
+})
