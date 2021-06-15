09e24fe44ce54afca721d07bd361e788d0502e26
gabrielodom <gabriel.odom@fiu.edu>
2021-06-15 15:59:11 -0400

    Closes Issue #38; closes Issue #39

--- R/parse.R
+++ R/parse.R
@@ -10,55 +10,83 @@
 #' @importFrom utils data
 #' @importFrom stats na.omit
 #' @importFrom magrittr %>%
+#' @importFrom stringr str_detect str_replace_all
+#' @importFrom tidytext unnest_tokens
+#' @importFrom dplyr mutate filter anti_join case_when
+#' @importFrom tibble tibble
 #' @export
 #'
 #' @examples
-#' \dontrun{
-#' parse(drug_vec)
-#' }
+#'   parse("Lortab and Percocet")
+#'
 
 parse <- function(drug_vec){
 
+  # browser()
+
   #binding vars to function
-  word <- drug <- for_token <- drug_stop_words <-  NULL
+  word <- drug <- for_token <- NULL
+  drug_stop_words <- DOPE::"drug_stop_words"
 
   ####Parse out drug names-------------------------------
-  #convert vector to df
-  drugs <- as.data.frame(drug_vec)
-
   #unnest tokens and remove special characters
   #get special cases
   #ex. Bup/Nx = Bup/Nx. = bup/nx = Bup Nx. = Bup Nx. = Bup Xx. = BupNx
   ## should be preserved - replace with any with spaces to have / so all
   ## are consistently bup/nx
   #ex. and combination of "speedball" also preserved
-  unnested_drugs <- drugs %>%
-    tidytext::unnest_tokens(word, drug_vec, token = "regex", pattern = "[,|-]",
-                            to_lower = TRUE) %>%
-    dplyr::mutate(drug = trimws(tolower(word)),
-                  #logic if a word can be tokenized or must remain as is
-                  is_token = stringr::str_detect(drug, "(?=.*bup)(?=.*nx|.*nal|.*nar)|(?=.*speed)(?=.*ball)"),
-                  #get the word to be tokenized
-                  for_token =  dplyr::case_when(stringr::str_detect(drug, "(?=.*bup)(?=.*nx|.*nal|.*nar)") == TRUE ~ "bup/nx",
-                                                stringr::str_detect(drug, "(?=.*speed)(?=.*ball)") == TRUE ~ "speedball",
-                                                #change any "/" to spaces
-                                                is_token == FALSE ~ gsub("/", " ", drug),
-                                                TRUE ~ drug),
+  unnestedDrugs_df <-
+    tibble(drug_vec) %>%
+    unnest_tokens(
+      word, drug_vec, token = "regex", pattern = "[,|-|+|&]", to_lower = TRUE
     ) %>%
-    tidytext::unnest_tokens(word, for_token, token = "regex", pattern = "[ ]",
-                            to_lower = TRUE) %>%
-    dplyr::anti_join(tidytext::stop_words) %>%
-    dplyr::filter(!grepl("[0-9]|[=]|[-]|[&]|[(]|[)]|[.]", word))
+    mutate(
+      drug = trimws(tolower(word))
+    ) %>%
+    # logic if a word can be tokenized or must remain as is
+    mutate(
+      is_token = str_detect(
+        drug, "(?=.*bup)(?=.*nx|.*nal|.*nar)|(?=.*speed)(?=.*ball)"
+      )
+    ) %>%
+    # get the word to be tokenized
+    mutate(
+      for_token = case_when(
+        str_detect(drug, "(?=.*bup)(?=.*nx|.*nal|.*nar)") == TRUE ~ "bup/nx",
+        str_detect(drug, "(?=.*speed)(?=.*ball)") == TRUE ~ "speedball",
+        #change any "/" to spaces
+        is_token == FALSE ~ gsub("/", " ", drug),
+        TRUE ~ drug
+      )
+    ) %>%
+    unnest_tokens(
+      word, for_token, token = "regex", pattern = "[ ]", to_lower = TRUE
+    ) %>%
+    # dplyr::filter(!grepl("[0-9]|[=]|[-]|[&]|[(]|[)]|[.]", word))
+    # The MS Word "smart quotes" are Unicode U+201C and U+201D
+    mutate(
+      word = str_replace_all(
+        word, '[0-9]|[=]|[(]|[)]|[.]|["]|[\\u201C]|[\\u201D]', ' '
+      ),
+      word = trimws(word)
+    ) %>%
+    filter(word != "") %>%
+    anti_join(tidytext::stop_words) %>%
+    filter(!(word %in% drug_stop_words))
 
-  #filter for drug specific stop-words
-  filtered_drugs <- gsub('"',"", unnested_drugs$word)
-  filtered_drugs <- dplyr::tibble(drug = filtered_drugs) %>%
-    #filter custom stopwords
-    dplyr::filter(!drug %in% drug_stop_words)
+
+  # #filter for drug specific stop-words
+  # filteredDrugs_char <- gsub('"',"", unnestedDrugs_df$word)
+  # filteredDrugs_df <-
+  #   dplyr::tibble(drug = filteredDrugs_char) %>%
+  #   #filter custom stopwords
+  #   dplyr::filter(!drug %in% drug_stop_words)
+
+
 
   #####Cleaning------------------------------
   # extra cleaning
-  clean_names <- sapply(filtered_drugs$drug, function(x){
+  clean_names <- sapply(unnestedDrugs_df$word, function(x){
     #remove anything after a / or - or = or "
     name <- sub("\\(.*|-.*|=.*|\".*", NA, x)
     #Remove any strings that have parentheses and content
@@ -70,7 +98,8 @@ parse <- function(drug_vec){
              #gsub("[^A-Za-z ]","",name)
            })
   })
-  clean_names <- clean_names %>%
+  clean_names <-
+    clean_names %>%
     unname() %>%
     na.omit()
   clean_names
--- tests/testthat/test-parse.R
+++ tests/testthat/test-parse.R
@@ -1,25 +1,119 @@
-###  Basic Test  ###
-testIn_vec <- c("1 oxycodone 1morphine",
-                 "Codeine",
-                 "Dilaudid",
-                " Dilaudid, Percocet, Norco,",
-                 "Dilaudid, Vicodin",
-                 "HYDROCODONE/OXYCODONE",
-                 "Heroin, Oxycodone, Methadone.",
-                 "Hydrocodone",
-                 "Hydrocodone/ibuprofen 7.5/200",
-                 "LORATAB, OXYCOCONE",
-                 "LORTAB",
-                 "Lortab",
-                 "Lortab and Percocet",
-                 "Lortab, Dilaudid",
-                 "METHADONE, LORTAB",
-                 "MORPHINE PERKS",
-                 "Methadone, Lorecet, Lorotab",
-                 "Methadone, Oxycodone",
-                 "Morphine")
 
-out_vec <- parse(testIn_vec)
-test_that("parse works", {
-  expect_equal(out_vec, parse(testIn_vec))
+# Test parse()
+# Gabriel and Ray
+# 2021-06-15
+
+
+######  Basic Tests  ##########################################################
+test_that("basic parse case sensitive", {
+  expect_equal(
+    "oxycodone", parse("Oxycodone")
+  )
 })
+
+test_that("stop words removed 1", {
+  expect_equal(
+    c("lortab", "percocet"), parse("Lortab and Percocet")
+  )
+})
+
+test_that("stop words removed 2", {
+  expect_equal(
+    "cocaine", parse("cocaine mg")
+  )
+})
+
+test_that("basic parse trim whitespace", {
+  expect_equal(
+    "oxycodone", parse(" oxycodone ")
+  )
+})
+
+test_that("basic multi parse", {
+  expect_equal(
+    c("dilaudid", "percocet", "norco"), parse(" Dilaudid, Percocet, Norco,")
+  )
+})
+
+test_that("remove backslash", {
+  expect_equal(
+    c("hydrocodone", "oxycodone"), parse("HYDROCODONE/OXYCODONE")
+  )
+})
+
+test_that("parse whitespace delim", {
+  expect_equal(
+    c("hydrocodone", "oxycodone"), parse("HYDROCODONE     OXYCODONE")
+  )
+})
+
+
+
+######  Bug Fixes  ############################################################
+test_that("remove numbers / punct", {
+  expect_equal(
+    c("oxycodone", "morphine"), parse("1 oxycodone 1morphine+")
+  )
+})
+
+test_that("split words on special symbols", {
+  expect_equal(
+    c("morphine", "cocaine"), parse("morphine+cocaine")
+  )
+})
+
+test_that("removed smart quotes", {
+  expect_equal(
+    "ambien", parse(" “a bunch” of Ambien ")
+  )
+})
+
+test_that("Bup/Nx is excepted", {
+  expect_equal(
+    c("cocaine", "bup/nx"), parse("cocaine, Bup/Nx")
+  )
+})
+
+sentence <- '
+  I was at a pary and I started with some Percocets and Vicodin.
+  I then had “a bunch” of Ambien and then some Barbiturate (no idea which).
+  My buddy Keith took promethazine (25mg), clonidine (0.1mg), but he needs a Bup/Nx
+'
+test_that("parsing a complex paragraph", {
+  expect_equal(
+    c(
+      "pary", "started", "percocets", "vicodin", "ambien", "barbiturate",
+      "idea", "buddy", "keith", "promethazine", "clonidine", "bup/nx"
+    ),
+    parse(sentence)
+  )
+})
+
+
+
+#
+# ###  Basic Test  ###
+# testIn_vec <- c("1 oxycodone 1morphine",
+#                  "Codeine",
+#                  "Dilaudid",
+#                 " Dilaudid, Percocet, Norco,",
+#                  "Dilaudid, Vicodin",
+#                  "HYDROCODONE/OXYCODONE",
+#                  "Heroin, Oxycodone, Methadone.",
+#                  "Hydrocodone",
+#                  "Hydrocodone/ibuprofen 7.5/200",
+#                  "LORATAB, OXYCOCONE",
+#                  "LORTAB",
+#                  "Lortab",
+#                  "Lortab and Percocet",
+#                  "Lortab, Dilaudid",
+#                  "METHADONE, LORTAB",
+#                  "MORPHINE PERKS",
+#                  "Methadone, Lorecet, Lorotab",
+#                  "Methadone, Oxycodone",
+#                  "Morphine")
+#
+# out_vec <- parse(testIn_vec)
+# test_that("parse works", {
+#   expect_equal(out_vec, parse(testIn_vec))
+# })
